16a50cd473534c584ddb6acdec3bf9e48ea373d3
putraw29 <125623530+putraw29@users.noreply.github.com>
2023-03-13 11:26:18 +0700

    Update geometer.py

--- geometer.py
+++ geometer.py
@@ -38,7 +38,7 @@ st.title("Geometer Apps")
 
 st.subheader("pilihlah bangun ruang dibawah ini")
 
-M = st.radio("pilih salah satu", ["kubus", "balok", "tabung", "bola", "kerucut"])
+M = st.selestbox("pilih salah satu", ["kubus", "balok", "tabung", "bola", "kerucut"])
 
 if M == "kubus":
     s = int(st.number_input("Masukkan sisi kubus: "))
