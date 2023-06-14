61318f6c29c9d8749d3c0a1db63029ba073f0074
putraw29 <125623530+putraw29@users.noreply.github.com>
2023-06-14 23:28:00 +0700

    coba.py

--- coba.py
+++ coba.py
@@ -1,35 +1,22 @@
 import streamlit as st
+from streamlit_chat import message
+from streamlit_extras.storage import file_state
 
-# Menyimpan pesan dari pengguna
-pesan_pengguna = []
+# Set the page title
+st.set_page_config(page_title='Chat App')
 
-def main():
-    # Judul aplikasi
-    st.title("Aplikasi Chat Sederhana")
+# Create a sidebar with some information about the chat app
+with st.sidebar:
+  st.title('Chat App')
+  st.text('This is a chat app that allows two people to chat with each other. All messages are saved for an unlimited amount of time.')
 
-    # Loop untuk mengirim dan menerima pesan
-    while True:
-        # Tampilan daftar pesan
-        with st.expander("Daftar Pesan"):
-            for pesan in pesan_pengguna:
-                st.write(pesan)
+# Create a chatbox where users can type their messages
+chatbox = st.text_input('Type your message here:')
 
-        # Input pesan pengguna
-        pesan = st.text_input("Pesan:", "")
+# Send the message to the other user
+if chatbox:
+  message(chatbox)
 
-        # Tombol Kirim
-        if st.button("Kirim"):
-            if pesan:
-                pesan_pengguna.append("Anda: " + pesan)
-                st.text_input("")
-
-        # Tombol Hapus
-        if st.button("Hapus Pesan"):
-            pesan_pengguna.clear()
-
-        # Tombol Keluar
-        if st.button("Keluar"):
-            break
-
-if __name__ == "__main__":
-    main()
+# Display the chat history
+messages = file_state.get('messages', [])
+st.markdown(messages)
