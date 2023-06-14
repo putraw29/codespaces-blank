de2d68a221629650807426d75405e047b1ba6f83
putraw29 <125623530+putraw29@users.noreply.github.com>
2023-06-14 23:55:02 +0700

    coba.py

--- coba.py
+++ coba.py
@@ -2,28 +2,27 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-# Create a data frame to store the chat messages
-messages = pd.DataFrame(columns=["sender", "message"])
+# Create a list to store the chat messages
+messages = []
 
-# Create a function to add a new message to the data frame
-def add_message(sender, message):
-    messages = messages.append({"sender": sender, "message": message}, ignore_index=True)
+# Create a function to add a new message to the chat
+def add_message(message):
+    messages.append(message)
 
 # Create a function to display the chat messages
 def display_messages():
+    st.write("Chat Messages")
     st.table(messages)
 
-# Create a function to start a new chat conversation
-def start_chat():
-    sender = st.text_input("Enter your name:")
-    message = st.text_input("Enter your message:")
-    add_message(sender, message)
-    display_messages()
+# Create a sidebar to select the user
+st.sidebar.title("Select User")
+user1 = st.sidebar.selectbox("User 1", ["User 1", "User 2"])
+user2 = st.sidebar.selectbox("User 2", ["User 1", "User 2"])
 
-# Start the chat app
-st.title("Chat App")
-st.write("This is a chat app where you can chat with another person.")
-st.button("Start Chat")
+# Create a button to send a message
+if st.button("Send Message"):
+    message = st.text_input("Message")
+    add_message(f"{user1}: {message}")
 
-if st.button("Start Chat"):
-    start_chat()
+# Display the chat messages
+display_messages()
