a20d1555ec75f84415e74ff1a45d1786bab6b46b
putraw29 <125623530+putraw29@users.noreply.github.com>
2023-06-14 23:17:10 +0700

    coba.py

--- coba.py
+++ coba.py
@@ -10,7 +10,7 @@ def main():
     # Loop untuk mengirim dan menerima pesan
     while True:
         # Tampilan daftar pesan
-        with st.beta_expander("Daftar Pesan"):
+        with st.expander("Daftar Pesan"):
             for pesan in pesan_pengguna:
                 st.write(pesan)
 
